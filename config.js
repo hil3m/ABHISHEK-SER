a1d4d2d8283b406b77e4d299923df55257e8433a
hil3m <byozdogru@gmail.com>
2023-09-08 15:33:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.owner = [
   [''] 
 ] //Number of owners
 
-global.mods = ['905443787048','919074692450'] 
-global.prems = ['905443787048', '919074692450', '919074692450']
+global.mods = ['905443787048','905530683158'] 
+global.prems = ['905443787048', '905530683158', '919074692450']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
