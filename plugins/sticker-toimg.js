a6bd48f16c8fdf45ba69d9d47269d77efd8ad080
hil3m <byozdogru@gmail.com>
2023-09-09 13:27:03 +0300

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -1,14 +1,14 @@
 import { webp2png } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `🎯 Reply To A Sticker With :\n\n *${usedPrefix + command}*`
+    const notStickerMessage = `🎯 bir stickeri alıntılaman gerek :\n\n *${usedPrefix + command}*`
     if (!m.quoted) throw notStickerMessage
     const q = m.quoted || m
     let mime = q.mediaType || ''
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*✅ Here You Go*', m)
+    await conn.sendFile(m.chat, out, 'out.png', '*✅ Tamam*', m)
 }
 handler.help = ['toimg <sticker>']
 handler.tags = ['sticker']
