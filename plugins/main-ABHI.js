7ee8d3a12216e672ae7df5319f06edf77012e261
hil3m <byozdogru@gmail.com>
2023-09-09 13:58:05 +0300

    Update main-ABHI.js

--- plugins/main-ABHI.js
+++ plugins/main-ABHI.js
@@ -1,17 +1,9 @@
 let handler = async m => m.reply(`
 
-≡  *𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁*   GROUPS
-▢ Join Public Bot Group And Support
-https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD
+≡  *HILEM*   GROUPS
 ─────────────
-▢ *Owner Telegram*
-https://t.me/Abhishek_Ser
- 
-▢ *YouTube*
-https://youtube.com/@comedymelodych8468
-
 ▢ *Instagram*
-https://instagram.com/abhishek_ser
+https://instagram.com/melihozdogru
 
 `.trim())
 handler.help = ['gpabhi']
