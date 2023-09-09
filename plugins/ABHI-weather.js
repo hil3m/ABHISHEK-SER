86ebcb65651337f4c5ea67922a3691e82ebc83ee
hil3m <byozdogru@gmail.com>
2023-09-09 13:03:50 +0300

    Update ABHI-weather.js

--- plugins/ABHI-weather.js
+++ plugins/ABHI-weather.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 let handler = async (m, { args }) => {
-if (!args[0]) throw "*Give A Place To Search*"
+if (!args[0]) throw "*Lütfen şehir bilgisi sağlayın*"
 try {
 const response = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`)
 const res = await response
@@ -12,7 +12,7 @@ const Minimum_Temperature = res.data.main.temp_min + "°C"
 const Maximum_Temperature = res.data.main.temp_max + "°C"
 const Humidity = res.data.main.humidity + "%"
 const Wind = res.data.wind.speed + "km/h"
-const wea = `「 📍 」PLACE: ${name}\n「 🗺️ 」COUNTRY: ${Country}\n「 🌤️ 」VIEW: ${Weather}\n「 🌡️ 」TEMPERATURE: ${Temperature}\n「 💠 」 MINIMUM TEMPERATURE: ${Minimum_Temperature}\n「 📛 」 MAXIMUM TEMPERATURE: ${Maximum_Temperature}\n「 💦 」HUMIDITY: ${Humidity}\n「 🌬️ 」 WINDSPEED: ${Wind}`
+const wea = `「 📍 」Şehir: ${name}\n「 🗺️ 」Ülke: ${Country}\n「 🌤️ 」Durum: ${Weather}\n「 🌡️ 」Sıcaklık: ${Temperature}\n「 💠 」 Min Sıcaklık: ${Minimum_Temperature}\n「 📛 」 Max Sıcaklık: ${Maximum_Temperature}\n「 💦 」Nem: ${Humidity}\n「 🌬️ 」 Rüzgar: ${Wind}`
 m.reply(wea)
 } catch {
 return "*ERROR*"}}
