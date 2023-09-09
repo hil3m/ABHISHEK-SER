cd6470f982e3b3fd3784968199b2616e47338db8
hil3m <byozdogru@gmail.com>
2023-09-09 15:56:05 +0300

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -3,8 +3,10 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
 ≡ *BAGIS*
+AKBANK
+TR96 0004 6008 9188 8000 0756 94
 HILEM BOT`
-let img = 'https://replicate.delivery/pbxt/QbP6Fh3ZXwKON9SCB70ERGwwgeeSbztwKIOIzhUeXFkwnFHiA/out.png'
+let img = 'https://i.imgur.com/O6LjRxB.jpg'
 conn.sendFile(m.chat, img, 'img.jpg', don, m)
 }
 
