d64dc7753a69e392462f73e3eb225d2cd418403a
hil3m <byozdogru@gmail.com>
2023-09-09 15:52:57 +0300

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -75,7 +75,7 @@ function clockString(ms) {
     return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
     
     function ucapan() {
-      const time = moment.tz('Asia/Kolkata').format('HH')
+      const time = moment.tz('Europe/Istanbul').format('HH')
       let res = "happy early in the day☀️"
       if (time >= 4) {
         res = "Good Morning 🌄"
