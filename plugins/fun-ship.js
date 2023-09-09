7f135bf5c2f3d59ac18837f63bf626d3e2f04b05
hil3m <byozdogru@gmail.com>
2023-09-09 15:34:07 +0300

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -5,7 +5,7 @@ function handler(m, { groupMetadata }) {
     let b
     do b = ps.getRandom()
     while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+    m.reply(`${toM(a)} ❤️ ${toM(b)}\nTebrikler Asiksiniz 💖🍻`, null, {
         mentions: [a, b]
     })
 }
